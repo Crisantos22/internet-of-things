df994e41e2251a92edb1980bdce1052368059737
jesus <jesuscrisantos22@gmail.com>
2026-03-26 07:38:55 -0600

    Actualizacion de archivos

--- ServidorWeb/ServidorWeb.ino
+++ ServidorWeb/ServidorWeb.ino
@@ -1,58 +1,63 @@
-#include <Wifi.h>
+#include <WiFi.h>
 #include <WebServer.h>
 
-const char* ssid= "ALUMNOS_TEC";
-const char* password= "";
+const char* ssid  = "ALUMNOS TEC";
+const char* password = "";
+const int ledPin = 14; // Consistencia en minúsculas
 
-//Inicializar Server
+// Inicializar server 
 WebServer server(80);
-const int ledPin = 14;
 
-void handleRoot(){
-  String html = " <h1> Controlar LED </h1>";
-  html += "<p><a href=\"/on\"><button> ON </button></a></p> ";
-  html += "<p><a href=\"/off\"><button> OFF </button></a></p> ";
-  server.send(200, "text/html",html);
-  }
-
-void handleOn(){
-  digitalWrite(ledPin, HIGH),
-  server.send(200, "text/html", "LED ENCENDIDO <br> <a href=\"/\"> VOLVER</a>");
-}
-
-void handleOff(){
-  digitalWrite(ledPin, HIGH),
-  server.send(200, "text/html", "LED APAGADO <br> <a href=\"/\"> VOLVER</a>");
-}
+// Prototipos de funciones (opcional pero recomendado)
+void handleRoot();
+void handleOn();
+void handleOff();
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
-  Serial.begin(115200);
+  Serial.begin(115200); // S mayúscula
   delay(400);
-  Wifi.begin(ssid, password);
+  
+  WiFi.begin(ssid, password); // WiFi con ambas mayúsculas
+  Serial.print("Conectando a Wi-Fi...");
 
-  Serial.print("CONECTANDO A WI-FI....");
-
-  while(Wifi.status() !=WI_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print("*");
+    Serial.print(".");
   }
-  Serial.println("\nCONECTADO");
-  Serial.println("\nIP DEL SERVIDOR: ");
-  Serial.println(Wifi.localIP());
+  
+  Serial.println("\nConectado");
+  Serial.print("IP del Servidor: ");
+  Serial.println(WiFi.localIP());
 
+  // Rutas
   server.on("/", handleRoot);
   server.on("/on", handleOn);
-  server.on("/off", handleOff);
-  server.begin();
+  server.on("/off", handleOff); // Corregido el nombre "hanldeOff"
   
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   server.handleClient();
-  
+}
+
+// Implementación de funciones
+void handleRoot() {
+  String html = "<h1>¡Controlar Led!</h1>"; // Falta punto y coma
+  html += "<p><a href=\"/on\"><button> ON </button></a></p>";
+  html += "<p><a href=\"/off\"><button> OFF </button></a></p>";
+  server.send(200, "text/html", html);
+}
+
+void handleOn() {
+  digitalWrite(ledPin, HIGH); // Era digitalWrite, no digitalWriter
+  server.send(200, "text/html", "LED ENCENDIDO <br><br><a href=\"/\">Volver</a>");
+}
+
+void handleOff() {
+  digitalWrite(ledPin, LOW); // Era digitalWrite
+  server.send(200, "text/html", "LED APAGADO <br><br><a href=\"/\">Volver</a>");
 }
